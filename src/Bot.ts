79b9b1fbcbe0ec2143553af9a877ad68a6c436ec
BadDeveloper <stijn.simons2@gmail.com>
2021-09-05 21:26:36 +0200

    added command base class

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,19 +1,26 @@
 import { Client } from 'discord.js';
 import { Logger } from 'winston';
 import winston = require('winston');
+import Command from './command/Command';
+import PingCommand from './command/impl/PingCommand';
 
 class Bot {
   private token?: string;
+  private prefix: string;
   private owner?: string;
 
   private client: Client;
+  private commands: Command[];
   private logger: Logger;
 
   constructor() {
     this.token = process.env.DISCORD_BOT_TOKEN;
+    this.prefix = process.env.BOT_PREFIX || '%';
     this.owner = process.env.DISCORD_BOT_OWNER;
 
     this.client = new Client({ intents: 'GUILD_MESSAGES' });
+
+    this.commands = [new PingCommand()];
 
     this.logger = winston.createLogger({
       level: 'info',
