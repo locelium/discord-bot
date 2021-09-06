36e4b07810bc98347d087a0602d8191115cd0fc8
BadDeveloper <stijn.simons2@gmail.com>
2021-09-06 02:00:05 +0200

    changed to discord.ts

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,21 +1,26 @@
-import { Client } from 'discord.js';
+import { Intents } from 'discord.js';
+import { Client } from '@typeit/discord';
 import { Logger } from 'winston';
 import winston = require('winston');
 
 class Bot {
-  private token?: string;
+  private token: string;
   private prefix: string;
-  private owner?: string;
+  private owner: string;
 
   private client: Client;
   private logger: Logger;
 
   constructor() {
-    this.token = process.env.DISCORD_BOT_TOKEN;
+    this.token = process.env.DISCORD_BOT_TOKEN || '';
     this.prefix = process.env.BOT_PREFIX || '%';
-    this.owner = process.env.DISCORD_BOT_OWNER;
+    this.owner = process.env.DISCORD_BOT_OWNER || '';
 
-    this.client = new Client({ intents: 'GUILD_MESSAGES' });
+    this.client = new Client({
+      intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+      classes: [`${__dirname}/*Discord.ts`, `${__dirname}/*Discord.js`],
+      silent: true
+    });
 
     this.logger = winston.createLogger({
       level: 'info',
@@ -36,7 +41,22 @@ class Bot {
   }
 
   async start(): Promise<void> {
+    this.client.once('ready', async () => {
+      await this.client.clearSlashes();
+      await this.client.initSlashes();
+    });
+
     await this.client.login(this.token);
+    this.client.on('ready', async () => {
+      await this.client.user?.setPresence({
+        activities: [
+          {
+            name: 'mining helium...',
+            type: 'PLAYING'
+          }
+        ]
+      });
+    });
     this.logger.info('Discord bot has logged in.');
   }
 }
